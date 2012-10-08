9660a2d284edf36057869926f20c27b7f473d5ec
Jeff Brown <brownjeffn@gmail.com>
2012-10-08 17:30:05 -0400

    upload 1

---
+++ includes/paydateranges.php
@@ -0,0 +1,9 @@
+<?php
+echo "<center>
+<a href='cnv_pay_wk1.php' class='rangebutton'>Week 1</a>
+<a href='cnv_pay_wk2.php' class='rangebutton'>Week 2</a>
+<a href='cnv_pay_wk3.php' class='rangebutton'>Week 3</a>
+<a href='cnv_pay_wk4.php' class='rangebutton'>Week 4</a>
+<a href='cnv_pay_wk5.php' class='rangebutton'>Week 5</a>
+</center>";
+?>
